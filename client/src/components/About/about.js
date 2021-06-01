117ef6220d6cf0d4677282f155d64455d479d2d5
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-06-01 11:41:41 +0200

    Todo collapse / Fade

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -1,7 +1,11 @@
-import React  from "react";
+import React, { useState, useEffect } from "react";
 import Header from '../Header/header';
+import {Fade} from 'react-bootstrap';
 
-const error404 = ({ match }) => {
+
+const Error404 = ({ match }) => {
+  const [open, setOpen] = useState(false);
+
     return (
         <>
         <Header isHomePage={false} isAboutPage={true}></Header>
@@ -17,28 +21,52 @@ const error404 = ({ match }) => {
         <div className="container">
            <div className="about mt-1rem">
                 <div className="col-md-10 dropdown mb-1rem">
-                    <header>
+                    <header
+                        onClick={() => setOpen(!open)}
+                        aria-controls="example-collapse-text"
+                        aria-expanded={open}
+                    >
                         <span>Fiabilité</span>
                         <img src={`${window.location.origin}/img/dropdown_arrow.png`} alt="Dropdown arrow"/>
                     </header>
-                    <p className="dropdown__text">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
+                <Fade in={open}>
+                    <div id="example-fade-text">
+                        <p className="dropdown__text">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
+                    </div>
+                </Fade>
+
+
+
+
                 </div>
                 <div className="col-md-10 dropdown mb-1rem">
-                    <header>
+                    <header
+                        onClick={() => setOpen(!open)}
+                        aria-controls="example-collapse-text"
+                        aria-expanded={open}
+                    >
                         <span>Respect</span>
                         <img src={`${window.location.origin}/img/dropdown_arrow.png`} alt="Dropdown arrow"/>
                     </header>
                     <p className="dropdown__text">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
                 </div>
                 <div className="col-md-10 dropdown mb-1rem">
-                    <header>
+                    <header
+                        onClick={() => setOpen(!open)}
+                        aria-controls="example-collapse-text"
+                        aria-expanded={open}
+                    >
                         <span>Service</span>
                         <img src={`${window.location.origin}/img/dropdown_arrow.png`} alt="Dropdown arrow"/>
                     </header>
                     <p className="dropdown__text">Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
                 </div>
                 <div className="col-md-10 dropdown mb-1rem">
-                    <header>
+                    <header
+                        onClick={() => setOpen(!open)}
+                        aria-controls="example-collapse-text"
+                        aria-expanded={open}
+                    >
                         <span>Sécurité</span>
                         <img src={`${window.location.origin}/img/dropdown_arrow.png`} alt="Dropdown arrow"/>
                     </header>
@@ -51,4 +79,4 @@ const error404 = ({ match }) => {
     )
 }
 
-export default error404;
+export default Error404;
