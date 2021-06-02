6d82888e4e9ed004d93a50eab603f958563fc114
GrimonprezAlexis <70689985+alexgrz.dev@users.noreply.github.com>
2021-06-03 00:35:15 +0200

    Deploy to Heroku

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const router = express.Router(); // Invoke the Express Router
 const apiRouter = require('./app/routes/router')(router); // Import the application end points/API
 app.use('/api', apiRouter); // Assign name to end points
 
+// serve up production assets
+app.use(express.static('client/build'));
+// let the react app to handle any unknown routes 
+// serve up the index.html if express does'nt recognize the route
+const path = require('path');
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// if not in production use the port 5000
 const PORT = process.env.PORT || 5000;
 console.log('server started on port:',PORT);
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
